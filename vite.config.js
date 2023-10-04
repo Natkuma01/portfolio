28f5922a8b506e3a8ca31f50030f0a71a525e7dc
Shimin <natkuma@outloook.com>
2023-10-04 19:43:25 +0800

    add yml file and edit viteConfig

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => {
+  const config = {
   plugins: [react()],
+  base: '/',
   css: {
     modules: {
       localConvention: "camelCase",
     },
-  },
+  }}
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+  return config
 });
