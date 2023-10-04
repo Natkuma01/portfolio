2c83cd4b557be60fe2f3ad1ae48154f264d35344
Shimin <natkuma@outloook.com>
2023-10-04 19:59:11 +0800

    edit vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
+export default defineConfig({
+  base:"/",
   plugins: [react()],
-  base: '/',
   css: {
     modules: {
       localConvention: "camelCase",
     },
-  }}
-  if (command !== 'serve') {
-    config.base = '/react-vite-gh-pages/'
-  }
-  return config
+  },
 });
